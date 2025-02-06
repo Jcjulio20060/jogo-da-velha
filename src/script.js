d3af16b005a89f05b57988c769f3a4e58c569470
Júlio <73503140+Jcjulio20060@users.noreply.github.com>
2025-02-06 14:26:56 -0300

    Início do game

--- src/script.js
+++ src/script.js
@@ -4,17 +4,32 @@ let board = new Array(9).fill(null);
 
 let btnPlay = document.getElementById('playGame');
 
-btnPlay.addEventListener('click', ()=>{
+btnPlay.addEventListener('click', () => {
     let player1 = document.getElementById('player1').value;
     let player2 = document.getElementById('player2').value;
-    if(player1 == '' || player2 == ''){ 
+    if (player1 == '' || player2 == '') { 
         alert('Player names are required');
         player1 = '';
         player2 = '';
         return;
     }
     players = [player1, player2];
-    document.getElementById('game').style.display = 'block';
-    document.getElementById('start').style.display = 'none';
-    // document.getElementById('currentPlayer').innerHTML = players[currentPlayer];
+    nextStep(0); // Chama a função para alternar as seções
+    currentPlayer = Math.floor(Math.random() * players.length);
 });
+
+function nextStep(step) {
+    switch(step) {
+        case 0:
+            document.getElementById('start').style.display = 'none';
+            document.getElementById('game').style.display = 'block';
+            break;
+        case 1:
+            document.getElementById('game').style.display = 'none';
+            document.getElementById('start').style.display = 'block';
+            break;
+    }
+}
+
+let play = document.getElementById('play');
+play.innerHTML = players[currentPlayer] + ' turn';
